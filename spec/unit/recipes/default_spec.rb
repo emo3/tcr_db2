0aacb2b7dfe90bebf2315db4d1dfd2e6849d721b
Ed Overton <e.overton@gmail.com>
2018-05-30 11:43:42 -0400

    spec version

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,11 +7,11 @@
 require 'spec_helper'
 
 describe 'tcr_db2::make_tcr' do
-  context 'When all attributes are default, on an Redhat 7.5' do
+  context 'When all attributes are default, on an Redhat 7.4' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'redhat', version: '7.5')
+      runner = ChefSpec::ServerRunner.new(platform: 'redhat', version: '7.4')
       runner.converge(described_recipe)
     end
 
